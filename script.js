04d265137179fb345c36131cfd775b549c8ff384
choupole <choupole13@gmail.com>
2023-12-18 11:15:52 +0100

    les structures conditionnelles

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const listeMots = ["Cacholot", "Pétunia", "Serviette"];
+
+let score = 0;
+
+let motUtilisateur = prompt("Entrez le mot : " + listeMots[0]);
+
+if(motUtilisateur === listeMots[0]) {
+    score += 1;
+}
+
+motUtilisateur = prompt("Entrez le mot : " + listeMots[1]); 
+
+if(motUtilisateur === listeMots[1]) {
+    score += 1;
+}
+
+motUtilisateur = prompt("Entrez le mot : " + listeMots[2] );
+
+if (motUtilisateur === listeMots[2]) {
+    score++;
+}
+console.log(score);
