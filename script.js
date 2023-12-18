e700a534fca687db696be10afb2f5d06c3db88f6
choupole <choupole13@gmail.com>
2023-12-18 16:44:10 +0100

    les boucles

--- script.js
+++ script.js
@@ -2,21 +2,13 @@ const listeMots = ["Cacholot", "Pétunia", "Serviette"];
 
 let score = 0;
 
-let motUtilisateur = prompt("Entrez le mot : " + listeMots[0]);
 
-if(motUtilisateur === listeMots[0]) {
-    score += 1;
-}
 
-motUtilisateur = prompt("Entrez le mot : " + listeMots[1]); 
+for (let i = 0 ; i < listeMots.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i]);
+    if (motUtilisateur === listeMots[i]) {
+        score++;
+    }
 
-if(motUtilisateur === listeMots[1]) {
-    score += 1;
-}
-
-motUtilisateur = prompt("Entrez le mot : " + listeMots[2] );
-
-if (motUtilisateur === listeMots[2]) {
-    score++;
 }
 console.log(score);
